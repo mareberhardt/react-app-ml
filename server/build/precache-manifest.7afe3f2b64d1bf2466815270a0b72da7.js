1d63fde086f323a766b3e7e0b75bbd9e064bd493
Martín Eberhardt <martineberhardt4@gmail.com>
2020-10-12 21:31:32 -0300

    Test React - Including build files

---
+++ server/build/precache-manifest.7afe3f2b64d1bf2466815270a0b72da7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a148ba3ce46d8c431a3d25838d5c1894",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fd2d57b192f3f8eebdb",
+    "url": "/static/css/main.8099d811.chunk.css"
+  },
+  {
+    "revision": "a0f572ca967134d7307d",
+    "url": "/static/js/2.f6c95511.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/static/js/2.f6c95511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fd2d57b192f3f8eebdb",
+    "url": "/static/js/main.86db15ab.chunk.js"
+  },
+  {
+    "revision": "228fa372d6f75a111782",
+    "url": "/static/js/runtime-main.e0d7a44d.js"
+  }
+]);
